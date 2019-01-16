599631505625e37b707c91a48a9c0a85d8fb8d4a
Mr.Winds <1299012718@qq.com>
2019-01-16 13:59:52 +0800

    成功添加了bootstrap

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloBoot />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HelloBoot from './components/HelloBootStrap.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloBoot
   }
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  devServer: {
+    port: 8080,
+    host: '10.62.137.199'
+  }
+}
