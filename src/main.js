dc890e9bfce31437d189b36ba0991f59b997277d
Mr.Winds <1299012718@qq.com>
2019-01-20 15:03:36 +0800

    添加了路由跳转页面

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+import routers from './router/router'
 import './assets/PcStyle.css'
 
 Vue.config.productionTip = false
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: routers
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
