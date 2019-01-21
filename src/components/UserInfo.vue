06a014708fcdce51df90340ae5084f0caba1126e
Mr.Winds <1299012718@qq.com>
2019-01-21 14:57:56 +0800

    加入了用户名

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>This is UserInfo</h1>
+  <div class="user-info">
+    <div class="panel panel-default user-info-panel">
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-md-2">
+            <label>用户名</label>
+          </div>
+          <div class="col-md-10">
+            <label>{{UserName}}</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
