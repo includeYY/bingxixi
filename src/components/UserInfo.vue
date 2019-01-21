54cdf7d8508af2a3a1a32efd15d8b785a352044d
Mr.Winds <1299012718@qq.com>
2019-01-21 16:09:02 +0800

    用户信息页完成

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -2,15 +2,59 @@
   <div class="user-info">
     <div class="panel panel-default user-info-panel">
       <div class="panel-body">
-        <div class="row">
-          <div class="col-md-2">
-            <label>用户名</label>
+        <form class="form-horizontal" role="form">
+          <div class="form-group">
+            <label class="col-md-1 control-label" for="user-name">用户名</label>
+            <div class="col-md-11">
+              <input type="text" class="form-control" readonly="readonly" id="user-name" v-bind:value="userName">
+            </div>
           </div>
-          <div class="col-md-10">
-            <label>{{UserName}}</label>
+          <div class="form-group">
+            <label class="col-md-1 control-label" for="age">年龄</label>
+            <div class="col-md-11">
+              <input type="text" class="form-control" id="age" v-bind:value="age">
+            </div>
           </div>
-        </div>
+          <div class="form-group">
+            <label class="col-md-1 control-label" for="sex">性别</label>
+            <div class="col-md-11">
+              <input type="text" class="form-control" id="sex" v-bind:value="sex">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-md-1 control-label" for="tel">联系方式</label>
+            <div class="col-md-11">
+              <input type="text" class="form-control" id="tel" v-bind:value="tel">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-md-1 control-label" for="address">常用地址</label>
+            <div class="col-md-11">
+              <input type="text" class="form-control" id="address" v-bind:value="address">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-md-offset-1 col-md-11">
+              <button type="submit" class="btn btn-success pull-right">保存</button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'UserInfo',
+  data () {
+    return {
+      userName: 'LePetit',
+      age: 21,
+      tel: '15810135518',
+      sex: '男',
+      address: '北京理工大学'
+    }
+  }
+}
+</script>
