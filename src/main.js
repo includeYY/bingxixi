b36f13dc0012244d032acce9bd41ac98d6b011b1
Mr.Winds <1299012718@qq.com>
2019-01-22 16:33:12 +0800

    调整全局样式

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Router from 'vue-router'
 import routers from './router/router'
-import './assets/PcStyle.css'
+import Cookies from 'js-cookie'
+import './assets/GlobalStyle.css'
 
 Vue.config.productionTip = false
 Vue.use(Router)
+
+Vue.prototype.Cookies = Cookies
 
 const router = new Router({
   mode: 'history',
