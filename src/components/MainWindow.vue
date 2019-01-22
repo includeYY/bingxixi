ea89a4b4bda08157fa7c8980379f64c17542826b
Mr.Winds <1299012718@qq.com>
2019-01-22 19:04:37 +0800

    增加几个示例数据

--- src/components/MainWindow.vue
+++ src/components/MainWindow.vue
@@ -46,6 +46,76 @@ var mBills = [
     product: 'IKBC',
     shop: '京东',
     target: '满499减200'
+  },
+  {
+    id: 0,
+    imgsrc: require('../assets/images/eleme.jpg'),
+    product: '汉堡王',
+    shop: '饿了么',
+    target: '满30减10'
+  },
+  {
+    id: 1,
+    imgsrc: require('../assets/images/jingdong.jpg'),
+    product: '汉堡王',
+    shop: '京东',
+    target: '满30减10'
+  },
+  {
+    id: 2,
+    imgsrc: require('../assets/images/meituan.jpg'),
+    product: '香河肉饼',
+    shop: '美团',
+    target: '满30减10'
+  },
+  {
+    id: 3,
+    imgsrc: require('../assets/images/taobao.jpg'),
+    product: 'MH世界',
+    shop: '淘宝',
+    target: '买二送一'
+  },
+  {
+    id: 4,
+    imgsrc: require('../assets/images/jingdong.jpg'),
+    product: 'IKBC',
+    shop: '京东',
+    target: '满499减200'
+  },
+  {
+    id: 0,
+    imgsrc: require('../assets/images/eleme.jpg'),
+    product: '汉堡王',
+    shop: '饿了么',
+    target: '满30减10'
+  },
+  {
+    id: 1,
+    imgsrc: require('../assets/images/jingdong.jpg'),
+    product: '汉堡王',
+    shop: '京东',
+    target: '满30减10'
+  },
+  {
+    id: 2,
+    imgsrc: require('../assets/images/meituan.jpg'),
+    product: '香河肉饼',
+    shop: '美团',
+    target: '满30减10'
+  },
+  {
+    id: 3,
+    imgsrc: require('../assets/images/taobao.jpg'),
+    product: 'MH世界',
+    shop: '淘宝',
+    target: '买二送一'
+  },
+  {
+    id: 4,
+    imgsrc: require('../assets/images/jingdong.jpg'),
+    product: 'IKBC',
+    shop: '京东',
+    target: '满499减200'
   }
 ]
 
