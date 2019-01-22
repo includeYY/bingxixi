57f3c2964d75669e527c143533c3760c4932dd1f
Mr.Winds <1299012718@qq.com>
2019-01-22 16:34:38 +0800

    为手机添加分类栏

--- src/components/Classify.vue
+++ src/components/Classify.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="pc-panel panel panel-default visible-lg visible-md">
-    <div class="table-responsive">
-      <table class="table classify-table">
-        <tbody>
-          <tr>
-            <td class="text-center">商家分类：</td>
-            <td><classify-button shop-name="全部"/></td>
-            <td><classify-button shop-name="美团"/></td>
-            <td><classify-button shop-name="饿了么"/></td>
-            <td><classify-button shop-name="淘宝"/></td>
-            <td><classify-button shop-name="京东"/></td>
-          </tr>
-        </tbody>
-      </table>
+  <div>
+    <div class="panel panel-default classify-panel visible-lg visible-md">
+      <div class="table-responsive">
+        <table class="table classify-table">
+          <tbody>
+            <tr>
+              <td class="text-center">商家分类：</td>
+              <td><classify-button shop-name="全部"/></td>
+              <td><classify-button shop-name="美团"/></td>
+              <td><classify-button shop-name="饿了么"/></td>
+              <td><classify-button shop-name="淘宝"/></td>
+              <td><classify-button shop-name="京东"/></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="panel panel-default classify-panel visible-xs visible-sm">
+      <div class="row">
+        <div class="col-xs-3 text-center classify-btn">
+          <img class="img-responsive center-block" src="../assets/images/eleme_logo.jpg">
+          <span class="text-center">饿了么</span>
+        </div>
+        <div class="col-xs-3 text-center classify-btn">
+          <img class="img-responsive center-block" src="../assets/images/meituan_logo.jpg">
+          <span class="text-center">美团</span>
+        </div>
+        <div class="col-xs-3 text-center classify-btn">
+          <img class="img-responsive center-block" src="../assets/images/taobao_logo.jpg">
+          <span class="text-center">淘宝</span>
+        </div>
+        <div class="col-xs-3 text-center classify-btn">
+          <img class="img-responsive center-block" src="../assets/images/jingdong_logo.jpg">
+          <span class="text-center">京东</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,10 +50,29 @@ export default {
 }
 </script>
 
-<style>
+<style scrop>
 
 .classify-table tbody tr td {
   vertical-align: middle;
+}
+
+.classify-panel {
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+.classify-btn {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 0px;
+  margin-right: 0px;
+}
+
+@media (min-width: 992px) {
+  .classify-panel{
+    margin-left: 50px;
+    margin-right: 50px;
+  }
 }
 
 </style>
