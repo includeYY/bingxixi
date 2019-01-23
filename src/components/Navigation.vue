2bef566e7587d1fd73e6924f0b2384bdd5f22137
Mr.Winds <1299012718@qq.com>
2019-01-23 19:18:17 +0800

    将底部导航独立出来

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -52,26 +52,6 @@
       </nav>
     </div>
 
-    <!-- 手机底部导航栏 -->
-    <nav class="navbar navbar-default navbar-fixed-bottom bottom-nav visible-xs visible-sm" role="navigation">
-      <div class="container-fluid bottom-container">
-        <div class="row">
-          <div class="col-xs-6 bottom-col">
-            <button type="button" class="btn btn-default navbar-btn bottom-btn">
-              <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
-              <div class="text-center">主页</div>
-            </button>
-          </div>
-          <div class="col-xs-6 bottom-col">
-            <button type="button" class="btn btn-default navbar-btn bottom-btn">
-              <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-              <div class="text-center">我的</div>
-            </button>
-          </div>
-        </div>
-      </div>
-    </nav>
-
     <!-- 登录模态框 -->
     <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLable" aria-hidden="true">
       <div class="modal-dialog">
@@ -201,26 +181,6 @@ export default {
   border: none;
   margin-top: 0px;
   margin-bottom: 0px;
-}
-
-.bottom-nav {
-  background-color: #ffffff;
-  border: none;
-}
-
-.bottom-container {
-  padding: 0px;
-}
-
-.bottom-col {
-  padding: 0px;
-}
-
-.bottom-btn {
-  width: 100%;
-  height: 100%;
-  margin: 0px;
-  border-radius: 0;
 }
 
 </style>
