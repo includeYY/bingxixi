f5d1538667f7fd92513bb6cbbd40e12eba41ef54
Mr.Winds <1299012718@qq.com>
2019-01-23 19:18:27 +0800

    添加手机下的模态框

--- src/components/UserSpace.vue
+++ src/components/UserSpace.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <router-link class="navbar-brand" to="/">并夕夕</router-link>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
@@ -14,6 +35,24 @@
           <transition name="right-fade">
             <router-view></router-view>
           </transition>
+        </div>
+      </div>
+    </div>
+
+    <button class="btn btn-default visible-xs visible-sm user-btn" data-toggle="modal" data-target="#UserModal">
+      <span class="glyphicon glyphicon-align-justify"></span>
+    </button>
+
+    <!-- 手机用户管理模态框 -->
+    <div class="modal fade" id="UserModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLable" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <ul class="nav nav-pill">
+            <li class="active sidebar-btn" v-on:click="ChangeState"><router-link to="/user">资料设置</router-link></li>
+            <li class="sidebar-btn" v-on:click="ChangeState"><router-link to="/user/message">消息通知</router-link></li>
+            <li class="sidebar-btn" v-on:click="ChangeState"><router-link to="/user/bill-info">正在进行的拼单</router-link></li>
+            <li class="sidebar-btn" v-on:click="ChangeState"><router-link to="/user/bill-info">已经结束的拼单</router-link></li>
+          </ul>
         </div>
       </div>
     </div>
@@ -43,5 +82,12 @@ export default {
 /* .slide-fade-leave-active for below version 2.1.8 */ {
   transform: translateX(10px);
   opacity: 0;
+}
+
+.user-btn {
+  position: fixed;
+  margin-top: 80%;
+  margin-left: 80%;
+  display: block;
 }
 </style>
