e6f7c1aefbb8d9e9fb1cc5089873c5a993b1f6fe
Mr.Winds <1299012718@qq.com>
2019-01-24 19:03:58 +0800

    完成登录注册的对接

--- src/components/LoginSignUp.vue
+++ src/components/LoginSignUp.vue
@@ -11,7 +11,7 @@
     <div class="tab-content">
       <div class="tab-pane fade in active" id="signup">
         <h1>免费注册</h1>
-        <h4 class="text-danger" v-if="error">{{message}}</h4>
+        <h4 v-if="isRegisterFail" class="text-danger">{{message}}</h4>
         <form>
           <div class="field-wrap">
             <input type="text" v-model="username" required autocomplete="off" placeholder="请输入用户名">
@@ -22,11 +22,21 @@
           <div class="field-wrap">
             <input type="password" v-model="passwordConfirm" required autocomplete="off" placeholder="请确认密码">
           </div>
-          <button type="submit" class="button button-block">注册</button>
+          <div class="field-wrap">
+            <input type="text" v-model="age" required autocomplete="off" placeholder="请输入年龄(可不填)">
+          </div>
+          <div class="field-wrap">
+            <input type="text" v-model="sex" required autocomplete="off" placeholder="请输入性别(可不填)">
+          </div>
+          <div class="field-wrap">
+            <input type="text" v-model="address" required autocomplete="off" placeholder="请输入地址(可不填)">
+          </div>
+          <button type="button" @click="RegisterClick" class="button button-block">注册</button>
         </form>
       </div>
       <div class="tab-pane fade" id="login">
         <h1>欢迎回来</h1>
+        <h4 v-if="isLoginFail" class="text-danger">{{message}}</h4>
         <form>
           <div class="field-wrap">
             <input type="text" v-model="username" required autocomplete="off" placeholder="请输入用户名">
@@ -49,32 +59,83 @@ export default {
   name: 'LoginSignUp',
   data () {
     return {
-      message: null,
-      error: false,
+      message: 'Hello',
+      isLoginFail: false,
+      isRegisterFail: false,
       username: null,
       password: null,
-      passwordConfirm: null
+      passwordConfirm: null,
+      age: null,
+      sex: null,
+      address: null
     }
   },
   methods: {
     LoginClick: function () {
-      this.$axios({
-        method: 'post',
-        url: this.Global.SERVER_URL.login,
-        data: this.qs.stringify({
-          username: this.username,
-          password: this.password
+      if (!this.username) {
+        this.isLoginFail = true
+        this.message = '请输入用户名'
+      } else if (!this.password) {
+        this.isLoginFail = true
+        this.message = '请输入密码'
+      } else {
+        this.isLoginFail = false
+        this.$axios({
+          method: 'post',
+          url: this.Global.SERVER_URL.login,
+          data: this.qs.stringify({
+            username: this.username,
+            password: this.password
+          })
+        }).then((response) => {
+          console.log(response)
+          if (response.data.status === 0) {
+            this.Global.UserInfo.userName = response.data.username
+            this.Global.UserInfo.isLogin = true
+            this.$router.push('/')
+          } else {
+            this.isLoginFail = true
+            this.message = response.data.msg
+          }
+        }).catch((error) => {
+          console.log(error)
         })
-      }).then((response) => {
-        console.log(response)
-        if (response.data.status === 0) {
-          this.Global.UserInfo.userName = response.data.username
-          this.Global.UserInfo.isLogin = true
-          this.$router.push('/')
-        }
-      }).catch((error) => {
-        console.log(error)
-      })
+      }
+    },
+    RegisterClick: function () {
+      if (!this.username) {
+        this.isRegisterFail = true
+        this.message = '请输入用户名'
+      } else if (!this.password) {
+        this.isRegisterFail = true
+        this.message = '请输入密码'
+      } else if (this.password !== this.passwordConfirm) {
+        this.isRegisterFail = true
+        this.message = '两次输入密码不一致'
+      } else {
+        this.isRegisterFail = false
+        this.$axios({
+          method: 'post',
+          url: this.Global.SERVER_URL.register,
+          data: this.qs.stringify({
+            username: this.username,
+            password: this.password,
+            age: this.age,
+            sex: this.sex,
+            address: this.address
+          })
+        }).then((response) => {
+          console.log(response)
+          if (response.data.status === 0) {
+            this.Global.UserInfo.userName = this.username
+            this.Global.UserInfo.isLogin = true
+            this.$router.push('/')
+          } else {
+            this.isRegisterFail = true
+            this.message = response.data.msg
+          }
+        })
+      }
     }
   }
 }
