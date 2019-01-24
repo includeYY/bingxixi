9aa19f9e557d35f91eaacff95000abb99ba8fde0
Mr.Winds <1299012718@qq.com>
2019-01-24 16:10:36 +0800

    增加错误提示

--- src/components/LoginSignUp.vue
+++ src/components/LoginSignUp.vue
@@ -11,32 +11,30 @@
     <div class="tab-content">
       <div class="tab-pane fade in active" id="signup">
         <h1>免费注册</h1>
-        <form action="/" method="post">
+        <h4 class="text-danger" v-if="error">{{message}}</h4>
+        <form>
           <div class="field-wrap">
-            <input type="text" required autocomplete="off" placeholder="请输入用户名">
+            <input type="text" v-model="username" required autocomplete="off" placeholder="请输入用户名">
           </div>
           <div class="field-wrap">
-            <input type="password" required autocomplete="off" placeholder="请输入密码">
+            <input type="password" v-model="password" required autocomplete="off" placeholder="请输入密码">
           </div>
           <div class="field-wrap">
-            <input type="password" required autocomplete="off" placeholder="确认密码">
+            <input type="password" v-model="passwordConfirm" required autocomplete="off" placeholder="请确认密码">
           </div>
           <button type="submit" class="button button-block">注册</button>
         </form>
       </div>
       <div class="tab-pane fade" id="login">
         <h1>欢迎回来</h1>
-        <form action="/" method="post">
+        <form>
           <div class="field-wrap">
-            <input type="text" required autocomplete="off" placeholder="请输入用户名">
+            <input type="text" v-model="username" required autocomplete="off" placeholder="请输入用户名">
           </div>
           <div class="field-wrap">
-            <input type="password" required autocomplete="off" placeholder="请输入密码">
+            <input type="password" v-model="password" required autocomplete="off" placeholder="请输入密码">
           </div>
-          <p class="forgot">
-            <a href="#">Forgot Password?</a>
-          </p>
-          <button class="button button-block">登录</button>
+          <button type="button" @click="LoginClick" class="button button-block">登录</button>
         </form>
       </div>
     </div>
@@ -48,6 +46,36 @@ import '../assets/sign-up-login-form/js/index'
 import '../assets/sign-up-login-form/css/style.css'
 
 export default {
-  name: 'LoginSignUp'
+  name: 'LoginSignUp',
+  data () {
+    return {
+      message: null,
+      error: false,
+      username: null,
+      password: null,
+      passwordConfirm: null
+    }
+  },
+  methods: {
+    LoginClick: function () {
+      this.$axios({
+        method: 'post',
+        url: this.Global.SERVER_URL.login,
+        data: this.qs.stringify({
+          username: this.username,
+          password: this.password
+        })
+      }).then((response) => {
+        console.log(response)
+        if (response.data.status === 0) {
+          this.Global.UserInfo.userName = response.data.username
+          this.Global.UserInfo.isLogin = true
+          this.$router.push('/')
+        }
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  }
 }
 </script>
